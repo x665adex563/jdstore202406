96df0397711b1099eb05014ed1040874d01567e7
Knight~ <x665adex563@gmail.com>
2024-06-07 12:12:01 +0800

    完成 建立結帳頁1

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,10 @@ class CartsController < ApplicationController
   def clean
     current_cart.clean!
     flash[:warning] = "已清空購物車"
-    redirect_to carts_path    
+    redirect_to carts_path
+  end
+
+  def checkout
+    @order = Order.new
   end
 end
